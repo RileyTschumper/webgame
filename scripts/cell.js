e39242680bf1250f89815b0c129f62d8277f81b4
Riley Tschumper <riley.tschumper@gmail.com>
2019-04-29 15:51:32 -0500

    made minesweeper (mostly)

--- scripts/cell.js
+++ scripts/cell.js
@@ -5,6 +5,10 @@ class Cell {
     this.x = i * size;
     this.y = j * size;
     this.size = size;
+    this.neighbors = 0;
+    this.mine = false;
+    this.flag = false;
+    this.shown = true;
   }
 
   draw(stage) {
@@ -12,8 +16,68 @@ class Cell {
     rect.graphics.beginFill("white");
     rect.graphics.beginStroke("black");
     rect.graphics.drawRect(this.x, this.y, this.size, this.size);
-    rect.graphics.endFill();
+    //rect.graphics.endFill();
 
     stage.addChild(rect);
+    if (this.shown) {
+      if (this.mine) {
+        var circle = new createjs.Shape();
+        circle.graphics.beginFill("gray");
+        circle.graphics.beginStroke("black");
+        circle.graphics.drawCircle(
+          this.x + this.size / 2,
+          this.y + this.size / 2,
+          this.size / 3
+        );
+        stage.addChild(circle);
+      } else {
+        rect = new createjs.Shape();
+        rect.graphics.beginFill("gray");
+        rect.graphics.beginStroke("black");
+        rect.graphics.drawRect(this.x, this.y, this.size, this.size);
+
+        if (this.neighbors != 0) {
+          var text = new createjs.Text(this.neighbors, "10px Arial", "black");
+          text.x = this.x + this.size / 2;
+          text.y = this.y + this.size / 2;
+          stage.addChild(text);
+        }
+        stage.addChildAt(rect, 0);
+      }
+    }
+  }
+
+  show() {
+    this.shown = true;
+  }
+
+  countNeighbors(grid, x, y) {
+    var total = 0;
+    if (this.mine) {
+      this.neighbors = undefined;
+      return;
+    } else {
+      for (var i = -1; i <= 1; ++i) {
+        for (var j = -1; j <= 1; ++j) {
+          if (i == 0 && j == 0) {
+            continue;
+          }
+          if (
+            i + x >= 0 &&
+            i + x < grid.length &&
+            j + y >= 0 &&
+            j + y < grid[0].length
+          ) {
+            console.log("I: " + i);
+            console.log("J: " + j);
+            if (grid[i + x][j + y].mine) {
+              total++;
+            }
+          }
+        }
+      }
+    }
+
+    this.neighbors = total;
   }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,65 @@
-var stageWidth = 640;
-var stageHeight = 480;
 var stage;
-var update = true;
-var shape1, shape2, shape3;
 
 function init() {
-  //new stage
-  stage = new createjs.Stage("canvas");
-
   var rows = 10;
   var cols = 10;
   var size = 30;
+  var numMines = 10;
 
-  grid = make2DArray(rows, cols);
+  document.getElementById("canvas").setAttribute("width", rows * size);
+  document.getElementById("canvas").setAttribute("height", cols * size);
+
+  //new stage
+  stage = new createjs.Stage("canvas");
+
+  var grid = make2DArray(rows, cols);
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < cols; j++) {
       grid[i][j] = new Cell(i, j, size);
     }
   }
 
+  //places mines
+  var placedMines = 0;
+  while (placedMines < numMines) {
+    var rand_i = Math.floor(Math.random() * rows);
+    var rand_j = Math.floor(Math.random() * cols);
+    console.log(rand_i);
+    console.log(rand_j);
+    if (!grid[rand_i][rand_j].mine) {
+      grid[rand_i][rand_j].mine = true;
+      placedMines++;
+      console.log(placedMines);
+    }
+  }
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      grid[i][j].countNeighbors(grid, i, j);
+    }
+  }
+
+  //draws each object on the canvas
   for (var i = 0; i < rows; i++) {
     for (var j = 0; j < cols; j++) {
       grid[i][j].draw(stage);
     }
   }
+
+  stage.on("stagemousedown", function(e) {
+    var x = e.stageX;
+    var y = e.stageY;
+    console.log(x);
+    console.log(y);
+    var i = Math.floor(x / size);
+    var j = Math.floor(y / size);
+
+    console.log("i: " + i);
+    console.log("j: " + j);
+    grid[i][j].show();
+    grid[i][j].draw(stage);
+    stage.update();
+  });
 
   stage.update();
 }
