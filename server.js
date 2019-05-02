486c2fd7d282c24df2c53ba758f78ce344b79c8d
Riley Tschumper <tsch3471@stthomas.edu>
2019-05-02 15:43:37 -0500

    added server login

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var url = require('url');
 var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3');
 
 var app = express();
@@ -17,15 +19,23 @@ var db = new sqlite3.Database(db_filename, sqlite3, (err) => {
 		console.log('Now connected to ' + db_filename);
 	}
 });
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(session({
-	secret: 'secret'
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+//app.use(express.static(public_dir));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/public/login.html'));
 });
 
-app.use(express.static(public_dir));
-
-app.get('/', (req, err) => {
-	req.sendFile(path.join(__dirname + 'public/login.html'));
+app.post('/auth', (req, res) => {
+	console.log(req.body.username);
+	console.log(req.body.password);
 });
 
 var server = app.listen(port);
