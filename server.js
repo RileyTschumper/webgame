5dafd7f571eb7089cb8f7a7d35a60338732a92d3
Riley Tschumper <tsch3471@stthomas.edu>
2019-05-05 23:40:29 -0500

    functioning login and account creation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3');
+var md5 = require('blueimp-md5');
 
 var app = express();
 var port = 8017;
@@ -33,10 +34,75 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/login.html'));
 });
 
+app.get('/home', (req, res) => {
+	if(req.session.loggedin){
+		res.send('Welcome back, ' + req.session.username + '!');
+	}
+	else{
+		res.send('Please login to view this page!');
+	}
+	res.end();
+});
+
 app.post('/auth', (req, res) => {
-	console.log(req.body.username);
-	console.log(req.body.password);
+	username = req.body.username;
+	password = req.body.password;
+	if(username != undefined && password != undefined){
+		hashedPassword = md5(password);
+		db.all('SELECT * FROM users WHERE username = ? AND password = ?', [username, hashedPassword], (err, rows) => {
+			if(err){
+				throw err;
+			}	
+			if(rows.length > 0){
+				req.session.loggedin = true;
+				req.session.username = username;
+				res.redirect('/home');
+			}
+			else {
+				res.send('Incorrect username and/or password');
+			}
+			res.end();
+		});
+	}
+	else {
+		res.send('Enter a username and password');
+		res.end();
+	}
+});
+
+app.post('/create', (req,res) => {
+	username = req.body.username;
+	password = req.body.password;
+	password_confirm = req.body.password_confirm;
+	if(password != password_confirm){
+		res.send('Passwords do not match. Re-enter your password.');
+		res.end();
+	}
+	else {
+		db.all('SELECT * FROM users WHERE username = ?', [username], (err, rows) => {
+			if(err) {
+				throw err;
+			}
+			if(rows.length > 0){
+				res.send('Username is taken. Please select a new username');
+				res.end();
+			}
+			else{
+				hashedPassword = md5(password);
+				console.log("Username: " + username);
+				console.log("Password: " + password);
+				db.run('INSERT INTO users(username, password) VALUES(?, ?)', [username, hashedPassword], (err) => {
+					if(err){
+						throw err;
+					}
+					console.log("added " + username + " to database");
+					req.session.loggedin = true;
+					req.session.username = username;
+					res.redirect('/home');
+				});		
+			}
+		});
+	}
 });
 
 var server = app.listen(port);
-
